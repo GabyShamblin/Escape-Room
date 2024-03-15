0a2fce279a6e8bcdc180ad0512b15d9ae7495eff
Chris Campanelli <christopherjcampanelli@gmail.com>
2024-03-15 02:17:26 -0400

    Chris Update 1
    Attempting to remove URP

---
+++ Assets/Scripts/Chris/Lantern.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Creates 
+/// </summary>
+/// 
+public class Lantern : MonoBehaviour
+{
+    private Light _light;
+
+    
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+    }
+
+    //Turns light on
+    public void LightOn()
+    {
+        _light.enabled = true;
+    }
+
+    //Turns light off
+    public void LightOff()
+    {
+        _light.enabled = false;
+        
+    }
+
+}
