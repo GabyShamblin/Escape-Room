ead4094283b8f7ecce5ba6fcd343d7498b1db316
travismiller22 <travismiller22@hotmail.com>
2024-03-14 23:17:54 -0400

    Revert "Merge branch 'Travii_Trunk'"
    This reverts commit 6fb26aaee1f2375903cdfdf43015b619be845082, reversing
    changes made to 011f0a1ea5015991b058ff2513015b0b70f7ef73.

--- Assets/Library Merger Folder/KeyAppears.cs
+++ Assets/Library Merger Folder/KeyAppears.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class KeyAppears : MonoBehaviour
 {
-    // Reference to the GameObject representing the key
     [SerializeField] GameObject _Key;
-    // Reference to the GameObject representing the audio
     [SerializeField] GameObject _audio;
 
     void Start()
     {
-	// Deactivates the key and audio GameObjects when the game starts
         _Key.SetActive(false);
         _audio.SetActive(false);
     }
-
-    // This function is called when another collider enters the trigger collider attached to this GameObject
     void OnTriggerEnter(Collider other)
     {
-	// Checking if the name of the GameObject entering the trigger is "Direct Interactor"
         if(other.gameObject.name == "Direct Interactor")
 	{
-	     // Activates the key and audio GameObjects if "Direct Interactor" enters the trigger
              _Key.SetActive(true);
              _audio.SetActive(true);
 	}
 	else
         {
-	    // Outputs a debug message indicating that a collision is not occurring
             Debug.Log("Collision is NOT happening.");
-	    // Outputs the name of the GameObject that collided with the trigger
 	    Debug.Log(other.gameObject.name);
         }        
     }
