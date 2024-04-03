c36e07e330ff46c0a0c955aa318a5db7c444577a
GabyShamblin <gabyshamblin@gmail.com>
2024-04-02 20:04:54 -0400

    Door & key animations

---
+++ Assets/StartBasementOpen.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBasementOpen : MonoBehaviour
+{
+    [SerializeField] private bool open = false;
+    private Animator anim;
+    [SerializeField] private GameObject key;
+    [SerializeField] private Material key_mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (anim == null) {
+            Debug.Log("Animator not found");
+        }
+        if (key == null) {
+            Debug.Log("Key not found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open & anim != null) {
+            key.GetComponent<MeshRenderer>().material = key_mat;
+            anim.Play("Base Layer.Basement_Open");
+            open = false;
+        }
+    }
+}
